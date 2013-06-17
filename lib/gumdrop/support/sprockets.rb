7ab25e18239eb7c993de7bce904496ed4bd47d18
M@ McCray <darthapo@gmail.com>
2013-06-16 23:37:49 -0500

    Bugfix: Sets correct load_path for sprockets.

--- lib/gumdrop/support/sprockets.rb
+++ lib/gumdrop/support/sprockets.rb
@@ -7,8 +7,9 @@ module Gumdrop::Support
       require 'sprockets'
       source_path = source_file || opt[:main] || opt[:from]
       env = ::Sprockets::Environment.new site.root
+      env.append_path File.expand_path(File.join site.source_path, File.dirname(source_path))
       env.append_path site.source_path
-      env.append_path File.dirname(source_path)
+      # env.append_path File.dirname(source_path)
       [opts[:paths]].flatten.each do |path|
         env.append_path(path) unless path.nil?
       end
